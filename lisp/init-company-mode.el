f0f7c3b25ba9e4a9e346845f5c1c708f84316333
Sharundaar <rabiller.donatien@hotmail.fr>
2017-06-11 13:22:16 -0400

    emacs settings for a rust dev env

---
+++ lisp/init-company-mode.el
@@ -0,0 +1,11 @@
+
+
+(require 'init-elpa)
+(require-package 'company)
+(require 'company)
+
+(setq company-tooltip-align-annotations t)
+(add-hook 'prog-mode-hook 'company-mode)
+
+(provide 'init-company-mode)
+
---
+++ lisp/init-rust.el
@@ -0,0 +1,31 @@
+
+
+(require 'init-elpa)
+(require-package 'company)
+(require-package 'racer)
+(require-package 'rust-mode)
+(require-package 'flycheck)
+(require-package 'flycheck-rust)
+
+(require 'company)
+(require 'racer)
+(require 'rust-mode)
+(require 'electric)
+(require 'eldoc)
+(require 'flycheck)
+(require 'flycheck-rust)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook  #'company-mode)
+(add-hook 'rust-mode-hook  #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(add-hook 'rust-mode-hook
+          '(lambda ()
+	     (setq racer-cmd (concat (getenv "CARGO_BIN") "\\racer.exe"))
+	     (setq racer-rust-src-path (getenv "RUST_SRC_PATH"))
+             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+	     (electric-pair-mode 1)))
+
+(provide 'init-rust)
+
